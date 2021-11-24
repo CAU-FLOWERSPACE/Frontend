fb5446bd050096489de30bd8df24710eaf9ca27c
ChoiJaeEun <poungki0823@gmail.com>
2021-11-24 13:19:16 +0900

    modify main.css directory

--- src/components/views/ARPage/webAR.js
+++ src/components/views/ARPage/webAR.js
@@ -11,7 +11,7 @@ document.write(<head>
 		<title>FlowerSpace</title>
 		<meta charset="utf-8"></meta>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"></meta>
-		<link type="text/css" rel="stylesheet" href="main.css"></link>
+		<link type="text/css" rel="stylesheet" href="../../../capstone_AR/main.css"></link>
 		<link rel="shortcut icon" href="#"></link>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	</head>);
